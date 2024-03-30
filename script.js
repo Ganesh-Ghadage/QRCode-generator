70d79b8b0e853185b0685fc938c65f0fc41f19a9
Ganesh Ghadage <36775013+Ganesh-Ghadage@users.noreply.github.com>
2024-03-30 16:42:24 +0530

    API handling

--- script.js
+++ script.js
@@ -5,8 +5,14 @@ let btn = document.querySelector("button");
 
 btn.addEventListener("click", async () => {
     if(qrText.value.length > 0){
-        qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?data=${qrText.value}`;
-        qrImg.classList.add("show");
+        fetch(`https://api.qrserver.com/v1/create-qr-code/?data=${qrText.value}`)   
+            .then(response => response.blob())
+            .then( (blob) => {
+                const imgURL = URL.createObjectURL(blob);
+                qrImg.src = imgURL;
+                qrImg.classList.add("show");
+            })
+            .catch((error) => console.error(error));
     }else{
         qrText.classList.add("error");
         setInterval(()=>{
